6de9459caced407ffa1140fd0a401821148d2a35
Mack Gallagher <wingsonthebus@gmail.com>
2022-05-30 11:10:46 -0500

    add handling for negative numbrers

--- macks_itoa.c
+++ macks_itoa.c
@@ -3,6 +3,14 @@
 #include <stdio.h>
 
 char * itoa(int num) {
+
+  int sign = 0;
+  if (num >= 0) {
+    sign = 1;
+  } else {
+    num = -num;
+  }
+
   int highest_power_of_ten_less_than_num = 0;
   while (pow(10,highest_power_of_ten_less_than_num) <= num) {
     highest_power_of_ten_less_than_num++;
@@ -15,6 +23,12 @@ char * itoa(int num) {
   int highest = pow(10,power)*multiplier;
 
   int i = 0;
+
+  if (sign == 0) {
+    ans[i] = '-';
+    i++;
+  }
+
   while (power >= 0) {
     highest = 0;
     multiplier = 0;
@@ -44,5 +58,7 @@ int main() {
   printf("My itoa returns %s for 234\n",itoa(234));
   printf("My itoa returns %s for 10\n",itoa(10));
   printf("My itoa returns %s for 100035\n",itoa(100035));
+  printf("My itoa returns %s for -8\n",itoa(-8));
+  printf("My itoa returns %s for -20\n",itoa(-20));
   return 0;
 }
